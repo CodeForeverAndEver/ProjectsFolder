5ea764dac42c0f70815c0b4e1d0b960bd33f1f73
SuperMaZingCoder <55718659+CodeForeverAndEver@users.noreply.github.com>
2020-10-22 21:59:09 -0400

    Refactor Code

--- DVDLogo/src/PhysicsCalculator.java
+++ DVDLogo/src/PhysicsCalculator.java
@@ -1,10 +1,7 @@
 public class PhysicsCalculator {
 
     public static boolean isBeyondBoundaries(float value, float distanceFromCenter, float max) {
-        if ((value - distanceFromCenter < 0 || value + distanceFromCenter > max)) {
-            return true;
-        }
-        return false;
+        return (value - distanceFromCenter) < 0) || (value + distanceFromCenter) > max;
     }
 
 }
